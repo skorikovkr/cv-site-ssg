8f9d4710aede74529012e0de9fdb963497097bec
Skorikov Kirill <skorikovkr@gmail.com>
2024-09-11 18:46:07 +0500

    Fixed files

--- src/pages/HomePage/MyStack.vue
+++ src/pages/HomePage/MyStack.vue
@@ -4,25 +4,25 @@ import MyStackCard from './MyStackCard.vue';
 
 const cards = ref([
     {
-        imgSrc: "/src/assets/vue-logo.png",
+        imgSrc: "/images/vue-logo.png",
         title: 'Vue',
         subtitle: 'Frontend framework',
         content: 'I use Vue for building UI for my web applications.'
     },
     {
-        imgSrc: "/src/assets/asp-net-logo.png",
+        imgSrc: "/images/asp-net-logo.png",
         title: 'ASP.NET',
         subtitle: 'Backend framework',
         content: 'I use ASP.NET (C#) for building backend for my applications.'
     },
     {
-        imgSrc: "/src/assets/tailwind-logo.svg",
+        imgSrc: "/images/tailwind-logo.svg",
         title: 'Tailwind',
         subtitle: 'CSS framework',
         content: 'I use tailwind for styling my application with classes.'
     },
     {
-        imgSrc: "/src/assets/pinia-logo.svg",
+        imgSrc: "/images/pinia-logo.svg",
         title: 'Pinia',
         subtitle: 'State manager',
         content: 'I use Pinia when I need to use global state for my components.'
