2f188e34b7a1514f927778d545659e4827b8f8cc
Skorikov Kirill <skorikovkr@gmail.com>
2025-03-30 21:29:54 +0500

    Updated contacts

--- src/components/DefaultLayout/DefaultLayoutSidebar.vue
+++ src/components/DefaultLayout/DefaultLayoutSidebar.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import type { SidebarProps } from '@/components/ui/sidebar'
 import {
   Sidebar,
   SidebarContent,
@@ -17,11 +16,14 @@ import {
 } from '@/components/ui/sidebar'
 
 import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '@/components/ui/collapsible'
-import { SquareTerminal, Home, ChevronRight } from 'lucide-vue-next'
+import { SquareTerminal, Home, ChevronRight, Contact } from 'lucide-vue-next'
 import DefaultLayoutSidebarHeader from './DefaultLayoutSiedbarHeader.vue'
-import { ref } from 'vue'
+import { ref, shallowRef } from 'vue'
 import { RouterLink } from 'vue-router'
 import ColorModePicker from '../ColorModePicker.vue'
+import TelegramIcon from '../icons/TelegramIcon.vue'
+import GitHubIcon from '../icons/GitHubIcon.vue'
+import SteamIcon from '../icons/SteamIcon.vue'
 
 const navLinks = ref([
   {
@@ -30,6 +32,18 @@ const navLinks = ref([
     items: [
       { title: 'Color correction', routeName: 'ColorCorrection' },
       { title: 'Metronome', routeName: 'Metronome' },
+    ],
+  },
+])
+
+const contacts = shallowRef([
+  {
+    title: 'Projects',
+    icon: Contact,
+    items: [
+      { title: 'Telegram', href: 'https://github.com/skorikovkr', icon: TelegramIcon },
+      { title: 'GitHub', href: 'https://t.me/skorikovkr', icon: GitHubIcon },
+      { title: 'Steam', href: 'https://steamcommunity.com/id/batiatrus', icon: SteamIcon },
     ],
   },
 ])
@@ -86,6 +100,56 @@ const navLinks = ref([
                       <RouterLink :to="{ name: subItem.routeName }">
                         <span>{{ subItem.title }}</span>
                       </RouterLink>
+                    </SidebarMenuSubButton>
+                  </SidebarMenuSubItem>
+                </SidebarMenuSub>
+              </CollapsibleContent>
+            </SidebarMenuItem>
+          </Collapsible>
+        </SidebarMenu>
+      </SidebarGroup>
+
+      <SidebarGroup>
+        <SidebarGroupLabel>Contacts</SidebarGroupLabel>
+        <SidebarMenu>
+          <Collapsible
+            v-for="item in contacts"
+            :key="item.title"
+            as-child
+            :default-open="true"
+            class="group/collapsible"
+          >
+            <SidebarMenuItem>
+              <CollapsibleTrigger as-child>
+                <SidebarMenuButton :tooltip="item.title">
+                  <component
+                    :is="item.icon"
+                    v-if="item.icon"
+                  />
+                  <span>{{ item.title }}</span>
+                  <ChevronRight
+                    class="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90"
+                  />
+                </SidebarMenuButton>
+              </CollapsibleTrigger>
+              <CollapsibleContent>
+                <SidebarMenuSub>
+                  <SidebarMenuSubItem
+                    v-for="subItem in item.items"
+                    :key="subItem.title"
+                  >
+                    <SidebarMenuSubButton as-child>
+                      <a
+                        target="_blank"
+                        :href="subItem.href"
+                      >
+                        <component
+                          :is="subItem.icon"
+                          v-if="subItem.icon"
+                          :size="16"
+                        />
+                        <span>{{ subItem.title }}</span>
+                      </a>
                     </SidebarMenuSubButton>
                   </SidebarMenuSubItem>
                 </SidebarMenuSub>
