89eb1ada224366482414b47f67c3193aa6621c86
skorikovkr <skorikovkr@gmail.com>
2025-09-23 23:55:54 +0500

    Added extraction node

---
+++ src/pages/ShaderGraphPage/ShaderGraphNodes/ExtractorNode.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="min-w-[125px] pl-6 mt-0">
+    <Label class="text-xs">Extraction prop</Label>
+    <NodeInput
+      v-model.lazy="value"
+      style="width: 85px"
+      v-prevent-pointer-movement
+    ></NodeInput>
+  </div>
+</template>
+
+<script setup>
+import { ref, watch } from 'vue'
+import NodeInput from './UI/NodeInput.vue'
+import { Label } from '@/components/ui/label'
+import { vPreventPointerMovement } from '../utils/PreventPointerDirectiveMove'
+
+const props = defineProps(['node'])
+
+const value = ref(props.node.options.propertyName ?? 'x')
+
+const emits = defineEmits(['updateNode'])
+
+watch(
+  [value],
+  () => {
+    emits('updateNode', props.node.id, {
+      options: {
+        propertyName: value.value,
+      },
+    })
+  },
+  {
+    deep: true,
+  },
+)
+</script>
--- src/pages/ShaderGraphPage/ShaderGraphNodes/ShaderGraphNodes.js
+++ src/pages/ShaderGraphPage/ShaderGraphNodes/ShaderGraphNodes.js
@@ -2,10 +2,12 @@ import UniformNode from './UniformNode.vue'
 import ValueNode from './ValueNode.vue'
 import FunctionInvokeNode from './FunctionInvokeNode.vue'
 import CustomNode from './CustomNode.vue'
+import ExtractorNode from './ExtractorNode.vue'
 
 export const ShaderGraphNodes = {
   value: ValueNode,
   uniform: UniformNode,
   'function-invoke': FunctionInvokeNode,
   'custom-node': CustomNode,
+  extractor: ExtractorNode,
 }
